7b8a84aada6af93b026ce97345d7e8b015b597e0
Kacper <kaceperloczek3@gmail.com>
2020-11-11 23:59:27 +0100

    GPS

---
+++ src/screens/GPS/GPS.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect, useRef } from 'react'
+import { Map, MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import 'leaflet/dist/leaflet.css';
+import './GPS.scss'
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import L from 'leaflet';
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+L.Marker.prototype.options.icon = DefaultIcon;
+
+
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.1/dist/leaflet.css" />
+
+function GPS() {
+    const [location, setLocation] = useState(null);
+    const mapEl = useRef(null);
+
+
+    useEffect(() => {
+
+        if ("geolocation" in navigator) {
+            console.log("Available");
+        } else {
+            console.log("Not Available");
+        }
+        navigator.geolocation.getCurrentPosition(function (position) {
+            setLocation([position.coords.latitude, position.coords.longitude]);
+        });
+
+
+    }, []);
+
+
+
+    return (
+        <div className='gps'>
+            {location &&
+                <MapContainer ref={mapEl} id='map' center={location} zoom={13} >
+                    <TileLayer
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+                    />
+                    <Marker position={location}>
+                        <Popup>
+                            <span>A pretty CSS3 popup Easily customizable.</span>
+                        </Popup>
+                    </Marker>
+                </MapContainer>
+            }
+
+            <span>
+                Twoje współrzędne: {location}
+            </span>
+        </div>
+    )
+}
+
+export default GPS
--- src/screensArray.js
+++ src/screensArray.js
@@ -3,12 +3,14 @@ import Home from './screens/Home/Home';
 import FormWithDateSaving from './screens/FormWithDateSaving/FormWithDateSaving';
 import Bst from './screens/BSTtime/Bst';
 import Camera from './screens/Camera/Camera';
+import GPS from './screens/GPS/GPS';
 // import { FALSE, TRUE } from 'node-sass';
 
 const screens = [
     { path: '/FormWithDateSaving', titleInList: 'Formularz z zapisem do bazy', header: <Header backButton={1} title='Formularz z zapisem do bazy' />, component: <FormWithDateSaving /> },
     { path: '/Camera', titleInList: 'Dostęp do aparatu', header: <Header backButton={1} title='Dostęp do aparatu' />, component: <Camera /> },
     { path: '/Bst', titleInList: 'Binarne drzewo poszukiwań - Pomiar czasu', header: <Header backButton={1} title='Binarne drzewo poszukiwań - Pomiar czasu' />, component: <Bst /> },
+    { path: '/GPS', titleInList: 'Lokalizacja', header: <Header backButton={1} title='Lokalizacja' />, component: <GPS /> },
     // { path: '/', titleInList: 'Home', header: <Header backButton={0} title='Home' />, component: <Home /> }
 ];
 
